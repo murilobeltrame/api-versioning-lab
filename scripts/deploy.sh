916d8b42488c2723ff035dc8c835219a8bb92550
Murilo Beltrame <murilobeltrame@gmail.com>
2023-04-24 20:50:42 -0300

    ...

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,8 +18,8 @@
 
 
 
-RESOURCE_GROUP=rg-enoughtrout
-SERVICE_NAME=apimenoughtrout
+RESOURCE_GROUP=rg-wittylobster
+SERVICE_NAME=apimwittylobster
 
 SPECIFICATION_FILES=src/swagger*.json
 API_NAME="Example" #$(cat $(ls $SPECIFICATION_FILES | head -n 1) | jq -r '.info.title')
@@ -35,14 +35,36 @@ if [ -z "$VERSION_SET_FOUND" ]; then
     VERSION_SET_ID=$(basename $(az apim api versionset create --display-name $API_NAME --resource-group $RESOURCE_GROUP --service-name $SERVICE_NAME --versioning-scheme "Segment" | jq -r '.id'))
 else 
     VERSION_SET_ID=$(basename $(echo "$VERSION_SET_FOUND" | jq -r '.id'))
-    echo "Theres a VersionSet created. VersionSet ID is $VERSION_SET_ID"
+    echo "Theres a VersionSet created. VersionSet ID is $VERSION_SET_ID."
 fi
 
 for FILE in $SPECIFICATION_FILES
 do
-    # echo "$f"
     API_VERSION=$(cat $FILE | jq -r '.info.version')
-    echo "Creating version $API_VERSION ..."
-    # TODO: Auto increment revisions
-    az apim api import --path $API_PATH --resource-group $RESOURCE_GROUP --service-name $SERVICE_NAME --specification-format "OpenApiJson" --api-version $API_VERSION --api-version-set-id $VERSION_SET_ID --display-name $API_NAME --specification-path $FILE --api-revision 2
+    API_VERSIONS=$(az apim api list --resource-group $RESOURCE_GROUP --service-name $SERVICE_NAME)
+    API_VERSION_FOUND=$(echo "${API_VERSIONS[@]}" | jq -r '.[] | select(.path == "'"$API_PATH"'" and .apiVersion == "'"$API_VERSION"'")')
+    if [ -z "$API_VERSION_FOUND" ]; then
+        echo "There isnt API Version created for $API_VERSION. Creating ..."
+        az apim api import --path $API_PATH \
+            --resource-group $RESOURCE_GROUP \
+            --service-name $SERVICE_NAME \
+            --specification-format "OpenApiJson" \
+            --api-version $API_VERSION \
+            --api-version-set-id $VERSION_SET_ID \
+            --display-name $API_NAME \
+            --specification-path $FILE
+    else
+        API_VERSION_ID=$(basename $(echo "$API_VERSION_FOUND" | jq -r '.id'))
+        echo "Theres a API Version created for $API_VERSION. Version ID is $API_VERSION_ID."
+        az apim api import --path $API_PATH \
+            --api-id $API_VERSION_ID \
+            --resource-group $RESOURCE_GROUP \
+            --service-name $SERVICE_NAME \
+            --specification-format "OpenApiJson" \
+            --api-version $API_VERSION \
+            --api-version-set-id $VERSION_SET_ID \
+            --display-name $API_NAME \
+            --specification-path $FILE
+    fi
+
 done
