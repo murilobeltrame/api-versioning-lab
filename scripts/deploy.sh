c7696b844d9318affd709c1c83ff33ecb011d930
Murilo Beltrame <murilobeltrame@gmail.com>
2023-04-24 16:52:11 -0300

    ...

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,14 +20,29 @@
 
 RESOURCE_GROUP=rg-enoughtrout
 SERVICE_NAME=apimenoughtrout
-API_NAME=Test
+
+SPECIFICATION_FILES=src/swagger*.json
+API_NAME="Example" #$(cat $(ls $SPECIFICATION_FILES | head -n 1) | jq -r '.info.title')
+API_PATH="example"
 VERSION_SET_ID=
+
+echo "Creating $API_NAME Api ..."
+
 VERSION_SETS=$(az apim api versionset list --resource-group $RESOURCE_GROUP --service-name $SERVICE_NAME)
 VERSION_SET_FOUND=$(echo "${VERSION_SETS[@]}" | jq -r '.[] | select(.displayName == "'"$API_NAME"'")')
 if [ -z "$VERSION_SET_FOUND" ]; then
-    echo "There isnt VersionSet created for $API_NAME. Creating ... "
+    echo "There isnt VersionSet created for $API_NAME. Creating ..."
     VERSION_SET_ID=$(basename $(az apim api versionset create --display-name $API_NAME --resource-group $RESOURCE_GROUP --service-name $SERVICE_NAME --versioning-scheme "Segment" | jq -r '.id'))
 else 
     VERSION_SET_ID=$(basename $(echo "$VERSION_SET_FOUND" | jq -r '.id'))
-    echo "Theres a VersionSet created. VersionSet id $VERSION_SET_ID"
+    echo "Theres a VersionSet created. VersionSet ID is $VERSION_SET_ID"
 fi
+
+for FILE in $SPECIFICATION_FILES
+do
+    # echo "$f"
+    API_VERSION=$(cat $FILE | jq -r '.info.version')
+    echo "Creating version $API_VERSION ..."
+    # TODO: Auto increment revisions
+    az apim api import --path $API_PATH --resource-group $RESOURCE_GROUP --service-name $SERVICE_NAME --specification-format "OpenApiJson" --api-version $API_VERSION --api-version-set-id $VERSION_SET_ID --display-name $API_NAME --specification-path $FILE --api-revision 2
+done
