7078bdf2516069711b30ede2db45b07336a5947e
Murilo Beltrame <murilobeltrame@gmail.com>
2023-04-29 12:08:01 -0300

    ....

--- src/spring/example/src/main/java/com/murilobeltrame/example/OpenApiConfiguration.java
+++ src/spring/example/src/main/java/com/murilobeltrame/example/OpenApiConfiguration.java
@@ -2,6 +2,10 @@ package com.murilobeltrame.example;
 
 import com.murilobeltrame.example.v1.WeatherForecastControllerV1;
 import com.murilobeltrame.example.v2.WeatherForecastControllerV2;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.customizers.ActuatorOpenApiCustomizer;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +16,11 @@ public class OpenApiConfiguration {
     public GroupedOpenApi apiV1() {
         return GroupedOpenApi.builder()
                 .group("v1")
+                .addOpenApiCustomizer(openApi -> {
+                    var info = new Info();
+                    info.setVersion("1.0");
+                    openApi.setInfo(info);
+                })
                 .packagesToScan(WeatherForecastControllerV1.class.getPackageName())
                 .build();
     }
@@ -20,6 +29,11 @@ public class OpenApiConfiguration {
     public GroupedOpenApi apiV2() {
         return GroupedOpenApi.builder()
                 .group("v2")
+                .addOpenApiCustomizer(openApi -> {
+                    var info = new Info();
+                    info.setVersion("2.0");
+                    openApi.setInfo(info);
+                })
                 .packagesToScan(WeatherForecastControllerV2.class.getPackageName())
                 .build();
     }
--- src/spring/example/src/main/java/com/murilobeltrame/example/v1/WeatherForecastControllerV1.java
+++ src/spring/example/src/main/java/com/murilobeltrame/example/v1/WeatherForecastControllerV1.java
@@ -1,7 +1,10 @@
 package com.murilobeltrame.example.v1;
 
 import com.murilobeltrame.example.WeatherForecast;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.NoSuchAlgorithmException;
@@ -12,6 +15,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/v1/weather-forecasts")
+@OpenAPIDefinition(info = @Info(version = "1.0"))
 public class WeatherForecastControllerV1 {
 
     private final String[] summaries = new String[]
@@ -19,7 +24,7 @@ public class WeatherForecastControllerV1 {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    @GetMapping("v1/weather-forecasts")
+    @GetMapping("/")
     List<WeatherForecast> getV1() throws NoSuchAlgorithmException {
         var forecasts = new ArrayList<WeatherForecast>();
         var random = SecureRandom.getInstanceStrong();
