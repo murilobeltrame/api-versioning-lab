c584ed5288ef6b8a24aa91034ebd668389a5b53b
Murilo Beltrame <murilo.beltrame@ambevtech.com.br>
2023-05-22 11:53:35 -0300

    ...

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+# 1. export swaggerfiles
+#   a. for netcore (apis-netcore.sh)
+#   b. for spring (apis-spring.sh)
+#   c. for fastapi (apis-fastapi.sh)
+
+# 2. provision apim environment (environment.sh)
+
+# 3. publish apis (publish.sh)
+
