070185e6f667d670cd977d50819ae6f44d9f1a5a
Murilo Beltrame <murilobeltrame@gmail.com>
2023-05-22 23:29:16 -0300

    ...

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
 # 1. export swaggerfiles
 #   a. for netcore (apis-netcore.sh)
+bash scripts/apis-netcore.sh "./src/netcore/Example.Api" "Example.Api.csproj" "$(pwd)/output" "api.yaml"
 #   b. for spring (apis-spring.sh)
+bash scripts/apis-spring.sh "./src/spring/example" "$(pwd)/output" "api.yaml"
 #   c. for fastapi (apis-fastapi.sh)
+bash scripts/apis-fastapi.sh "./src/fastapi" "$(pwd)/output" "api.yaml"
 
 # 2. provision apim environment (environment.sh)
 
